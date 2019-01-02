7314a36a13cbf80e18c7caee3ae829d253e5e09a
Harry Hunt <developingharry@outlook.com>
2019-01-02 14:32:52 +0000

    takes santas, needs to assign giftees

--- js/app.js
+++ js/app.js
@@ -1,4 +1,27 @@
+function Santa(name,giftee) {
+    var self = this;
+
+    self.name = ko.observable(name);
+    self.giftee = ko.observable(giftee);   
+}
+
+
 var ViewModel = function() {
+	var self = this;
+
+	self.santas = ko.observableArray([
+		new Santa("Mary"),
+		new Santa("Dave")
+	]);
+
+	self.addSanta = function() {
+        self.santas.push(new Santa("",""));
+    }
+
+    self.assignGiftees = function() {
+    	// TODO: add mechanism to assign giftees
+    	console.log("assigning giftees...");
+    }
 };
  
 ko.applyBindings(new ViewModel());
