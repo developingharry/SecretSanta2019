f5cbe31bbe23d4a44ae593a251dd64f1e446b2e9
Harry Hunt <developingharry@outlook.com>
2019-01-20 20:03:29 +0000

    fully functional now, just needs optimising and styling

--- js/app.js
+++ js/app.js
@@ -1,21 +1,10 @@
-function getQueryVariable(variable)
-{
-       var query = window.location.search.substring(1);
-       var vars = query.split("&");
-       for (var i=0;i<vars.length;i++) {
-               var pair = vars[i].split("=");
-               if(pair[0] == variable){return pair[1];}
-       }
-       return(false);
-}
-
 // basic encryption system using Base64
 function encrypt(name) {
-	return window.btoa(name);
+    return window.btoa(name);
 }
 
 function decrypt(name) {
-	return window.atob(name);
+    return window.atob(name);
 }
 
 // global variable to store validation errors from input fields.
@@ -45,32 +34,38 @@ ko.extenders.required = function(target, overrideMessage) {
     return target;
 };
 
-
 // prototype santa, or "gifter"
 function Santa(name) {
     var self = this;
 
     // extended observable to track name, and give error if one is not entered
     self.name = ko.observable(name).extend({
-        required: "Please enter a first name"
+        required: ""
     });
 
+    // each "santa"'s individual gift recipient
     self.giftee = ko.observable();
 
+    // the budget is a global figure, so to speak, but it makes sense for binding
+    // to link each object to it.
     self.budget = ko.observable(ViewModel.santaBudget);
 
+    // the giftee encrypted, so that the results can be hidden from the user if they wish
+    // to participate in said Secret Santa event.
     self.encGiftee = ko.computed(function() {
         return encrypt(self.giftee());
     }, self);
 
-    self.unEncGiftee = ko.computed(function() {
-        return decrypt(self.encGiftee());
-    }, self);
+    // prefix stored for easy updating in case of moving hosts.
+    self.urlPrefix = ko.observable("http://127.0.0.1/");
 
+    // the full web address that's given to each santa
     self.secretUrl = ko.computed(function() {
-        return ("index.htm?santa=" + self.name() + "&giftee=" + self.encGiftee() + "&secret=true" + "&budget=" + ViewModel.santaBudget);
+        return (self.urlPrefix() + "index.htm?santa=" + self.name() + "&giftee=" + self.encGiftee() + "&secret=true" + "&budget=" + ViewModel.santaBudget);
     }, self);
 
+    // function to copy the above url to the clipboard.
+    // this is done by making an invisible input field, populating it with the text, copying and deleting it.
     self.copyUrl = function(id) {
         var elementId = id;
         var hiddenInput = document.createElement("input");
@@ -81,10 +76,7 @@ function Santa(name) {
         document.body.removeChild(hiddenInput);
         alert("copied");
     }
-
 }
-
-
 
 var ViewModel = function() {
     var self = this;
@@ -92,82 +84,68 @@ var ViewModel = function() {
     // to track whether anyone has been matched with themself:
     var matchesFound = false;
 
+    // main array of Santas,starting with one empty entry to get user started.
     self.santas = ko.observableArray([
         new Santa("")
     ]);
 
-    // filtered copy of above array, showing only unique names.
-    // This is so that validation can check for duplicates
-
-
-
-
-
-
-    // self.uniqueSantas = ko.computed(function() {
-    //         var filteredArray = [];
-    //         var i;
-    //         $.each(self.santas(), function (index, item) {
-    //              var alreadyAdded = false;
-    //              for (i in filteredArray) {
-    //                   if (filteredArray[i].name == item.name) {
-    //                         console.log("i found a matching name within the list");
-    //                         alreadyAdded = true;
-    //                   }
-    //              }
-    //               if (!alreadyAdded) {
-    //                     console.log("looks fine to me boss");
-    //                    filteredArray.push(item);
-    //               }
-    //         });
-    //         return  filteredArray;
-
-    //     });
-
-
-    self.justSantaNames = ko.computed(function() {
-        var names = ko.utils.arrayMap(this.santas(), function(item) {
-        return item.name();
-    });
-    return names.sort();
-    }, self);
-
-
-    self.uniqueSantaNames = ko.dependentObservable(function() {
-    return ko.utils.arrayGetDistinctValues(self.justSantaNames()).sort();
-    }, self);
-
-    self.santaCount = ko.observable(self.santas().length);
-
-    // self.uniqueCount = ko.observable(self.uniqueSantas().length);
-
-    self.santaBudget = ko.observable();
-
-    self.giftees = ko.observableArray([]);
-
+    // the main function for adding Santas to the array
     self.addSanta = function() {
         self.santas.push(new Santa(""));
         self.santaChange(true);
     };
 
-    self.resultsOption = ko.observable("showResults");
-
-    // small tweak to avoid old results being accesible when amendments are made such as adding/removing santas
-    self.santaChange = ko.observable(false);
-
-    self.finished = ko.observable(false);
-
+    //...and the function for removing them (done by clicking the recycle bin button)
     self.removeSanta = function(data) {
         self.santaChange(true);
         self.santas.remove(data);
     };
 
-    self.cleanUrl = function() {
-        console.log("cleaning url");
-        var clean_url = location.protocol + "//" + location.host + location.pathname;
-        window.history.replaceState({}, document.title, clean_url);
-    };
+    // empty array of giftees, or recipients.  this will later be populated with 
+    // the contents of the santas array, and shuffled.
+    self.giftees = ko.observableArray([]);
 
+    // function to read the bespoke urls sent to each Santa
+    self.getQueryVariable = function(variable) {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split("=");
+            if (pair[0] == variable) {
+                return pair[1];
+            }
+        }
+        return (false);
+    }
+
+    // copy of santas array, showing only names...
+
+    self.justSantaNames = ko.computed(function() {
+        var names = ko.utils.arrayMap(this.santas(), function(item) {
+            return item.name();
+        });
+        return names.sort();
+    }, self);
+
+    // ...filtered copy of above array, showing only unique names.
+    // This is so that validation can check for duplicates
+
+    self.uniqueSantaNames = ko.dependentObservable(function() {
+        return ko.utils.arrayGetDistinctValues(self.justSantaNames()).sort();
+    }, self);
+
+    // record of what the max spend is on this secret santa, if the user wishes to set one.
+    self.santaBudget = ko.observable();
+
+    // to hold the value of the "would you like to see the results" radio button
+    self.resultsOption = ko.observable("showResults");
+
+    // small tweak to avoid old results being accesible when amendments are made such as adding/removing santas
+    self.santaChange = ko.observable(false);
+
+    // records whether the assignment of giftees is truly finished, so that the user 
+    // doesn't see the "work in progress" as the results are generated.
+    self.finished = ko.observable(false);
 
     // a "Knuth" shuffle, for use and reuse as required
     self.shuffle = function(a) {
@@ -200,12 +178,13 @@ var ViewModel = function() {
         var a = self.santas();
         var b = self.giftees();
         for (var i = 0; i < a.length; i++) {
-        	a[i].giftee(b[i].name());
+            a[i].giftee(b[i].name());
         }
     }
 
+    // self-auditing on the shuffle to check whether anyone has been matched with themself
     self.checkShuffle = function() {
-    	// compare the two arrays (gifters and giftees) with a fairly simple loop
+        // compare the two arrays (gifters and giftees) with a fairly simple loop
         var a = self.santas();
         var b = self.giftees();
         matchesFound = false;
@@ -219,67 +198,55 @@ var ViewModel = function() {
         }
     }
 
+    // master function of the "FINISHED"
+    // button, which shuffles until appropriate matches made.
     self.assignGiftees = function() {
+        // counting the total santas
         var allSantas = self.justSantaNames().length;
+        // counting the UNIQUE santas
         var uniqueSantas = self.uniqueSantaNames().length;
-      if(allSantas == uniqueSantas) {
-                    // initial shuffle before testing
-        // console.log("there are " + self.santaCount() + "santas and " + self.uniqueCount() + " unique santas.");
-        // console.log("the santas are" + self.santas());
-        // console.log("the unique santas are" + self.uniqueSantas());
-        console.log("oh and these santas" + self.justSantaNames() + self.justSantaNames().length);
-        console.log("oh and these uniques" + self.uniqueSantaNames() + self.uniqueSantaNames().length);
-
-        self.giftees(self.shuffle(self.santas().slice()));
-        // shuffle and test
-        self.shuffleTilCorrect();
-        self.finished(true);
-        self.santaChange(false);
+        // checking if there are any duplicates, before proceeding with the shuffle, to avoid infinite looping.
+        if (allSantas == uniqueSantas) {
+            // initial shuffle before testing
+            self.giftees(self.shuffle(self.santas().slice()));
+            // shuffle and test
+            self.shuffleTilCorrect();
+            self.finished(true);
+            // this variable shows whether changes have been made since last submission, and hides the results until
+            // the point this function changes it back.
+            self.santaChange(false);
         } else {
             alert("Sorry, I found two or more matching names in there.  Maybe add the surname to avoid confusion?")
         };
-    	// // initial shuffle before testing
-     //    self.giftees(self.shuffle(self.santas().slice()));
-     //    // shuffle and test
-     //    self.shuffleTilCorrect();
-     //    self.finished(true);
-     //    self.santaChange(false);
-
 
     }
 
-    // self.santaData = getQueryVariable("santa");
-
+    // reads the bespoke url to check who the santa is.  as with the budget, this also
+    // removes any url formatting, just in case spaces/characters have been used
     self.santaData = function() {
-        return decodeURIComponent(getQueryVariable("santa"));
-    }
-    self.secretData = function() {
-        return getQueryVariable("secret");
+        return decodeURIComponent(self.getQueryVariable("santa"));
     }
 
-    self.showBudget = function() {
-        return decodeURIComponent(getQueryVariable("budget"));
-    }
-
+    // as above, this one doesn't need the characters removed though as it will be an encoded string already.
     self.gifteeData = function() {
-        return decrypt(getQueryVariable("giftee"));
+        return decrypt(self.getQueryVariable("giftee"));
+    }
+    // simple method to display either the hidden, "secret" page, or the main submission page.
+    // the information is again taken from the url.
+    self.secretData = function() {
+        return self.getQueryVariable("secret");
     }
 
-    self.copyToClipboard = function(event) {
-        // take the mouse target's id
-        alert(event.currentTarget.id);
-        // store it
-        var elementId = event.currentTarget.id;
-        // make a hidden input box for copying and pasting
-        var hiddenInput = document.createElement("input");
-        // fill it and remove it
-        hiddenInput.setAttribute("value", document.getElementById(elementId).innerHTML);
-        document.body.appendChild(hiddenInput);
-        hiddenInput.select();
-        document.execCommand("copy");
-        document.body.removeChild(hiddenInput);
-        alert("copied");
-    } 
- };
+    // function to show budget if it's been set, and a custom message if it hasn't.
+    self.showBudget = function() {
+        var budget = decodeURIComponent(self.getQueryVariable("budget"));
+        if (budget === "undefined") {
+            return ("unlimited, apparently!");
+        } else {
+            return budget;
+        }
+    }
+
+};
 
 ko.applyBindings(new ViewModel());
